87c084189c2cd6d339ee86fc80a6d5b637cd348d
Matheussvini <matheussvini@outlook.com>
2023-05-12 02:10:01 -0300

    feat: getHotelsWithrooms return room occupation also

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,8 +1,10 @@
+import { Room } from '@prisma/client';
 import hotelRepository from '@/repositories/hotel-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 import { notFoundError } from '@/errors';
 import ticketsRepository from '@/repositories/tickets-repository';
 import { cannotListHotelsError } from '@/errors/cannot-list-hotels-error';
+import bookingRepository from '@/repositories/booking-repository';
 
 async function listHotels(userId: number) {
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
@@ -26,14 +28,24 @@ async function getHotels(userId: number) {
   return hotels;
 }
 
+type RoomWithOcupation = Room & { ocupation: number };
+
 async function getHotelsWithRooms(userId: number, hotelId: number) {
   await listHotels(userId);
 
   const hotel = await hotelRepository.findRoomsByHotelId(hotelId);
-
   if (!hotel || hotel.Rooms.length === 0) {
     throw notFoundError();
   }
+
+  const hotelWithRoomsAndOcupation = await Promise.all(
+    hotel.Rooms.map(async (room) => {
+      const ocupation = (await bookingRepository.findByRoomId(room.id)).length;
+      return { ...room, ocupation } as RoomWithOcupation;
+    }),
+  );
+  hotel.Rooms = hotelWithRoomsAndOcupation;
+
   return hotel;
 }
 
